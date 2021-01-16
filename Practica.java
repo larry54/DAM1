da386726871f64ebfb439b97920ab3502eb25cae
Miranda <75443561+larry54@users.noreply.github.com>
2021-01-16 13:23:43 +0100

    Add files via upload

---
+++ Practica.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexm
+ */
+public class Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu edad");
+        int edad=sc.nextInt();
+        System.out.println("Tu edad es "+edad);
+        
+    }
+    
+}
